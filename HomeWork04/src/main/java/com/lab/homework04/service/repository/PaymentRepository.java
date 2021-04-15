33eafdfa6be11b23fc63cab34df4ba983ca033a3
Taras-Chaban <gold.bender2001@gmail.com>
2021-04-15 13:33:05 +0300

    add PaymentRepository

---
+++ HomeWork04/src/main/java/com/lab/homework04/service/repository/PaymentRepository.java
@@ -0,0 +1,13 @@
+package com.lab.homework04.service.repository;
+
+import com.lab.homework04.service.model.Payment;
+
+public interface PaymentRepository {
+    Payment getPayment(String productCode);
+
+    Payment createPayment(Payment payment);
+
+    Payment updatePayment(String productCode, Payment payment);
+
+    void deletePayment(String productCode);
+}
