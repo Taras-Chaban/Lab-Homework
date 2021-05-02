cc72aa2b2726fa87a4311ec32bf178e888631536
Taras-Chaban <gold.bender2001@gmail.com>
2021-05-02 04:14:10 +0300

    add UserApi

---
+++ Homework5/src/main/java/com/lab/homework5/service/api/UserApi.java
@@ -0,0 +1,24 @@
+package com.lab.homework5.service.api;
+
+import com.lab.homework5.service.controller.model.UserModel;
+import com.lab.homework5.service.dto.UserDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+public interface UserApi {
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping(value = "/{email}")
+    UserModel getUser(@PathVariable String email);
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    UserModel createUser(@RequestBody UserDto userDto);
+
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping(value = "/{emailUp}")
+    UserModel updateUser(@PathVariable String emailUp, @RequestParam UserDto userDto);
+
+    @DeleteMapping(value = "/{emailDel}")
+    ResponseEntity<Void> deleteUser(@PathVariable String emailDel);
+}
--- Homework5/src/main/java/com/lab/homework5/service/controller/UserController.java
+++ Homework5/src/main/java/com/lab/homework5/service/controller/UserController.java
@@ -1,12 +1,12 @@
 package com.lab.homework5.service.controller;
 
+import com.lab.homework5.service.api.UserApi;
 import com.lab.homework5.service.controller.assembler.UserAssembler;
 import com.lab.homework5.service.controller.model.UserModel;
 import com.lab.homework5.service.dto.UserDto;
 import com.lab.homework5.service.service.impl.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,36 +15,33 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/users")
-public class UserController {
+public class UserController implements UserApi {
     private final UserServiceImpl userService;
     private final UserAssembler userAssembler;
 
-    @ResponseStatus(HttpStatus.OK)
-    @RequestMapping(value = "/{email}", method = RequestMethod.GET)
-    public UserModel getUser(@PathVariable String email) {
+    @Override
+    public UserModel getUser(String email) {
         log.info("Getting user with email{}", email);
         UserDto userDto = userService.getUser(email);
         return userAssembler.toModel(userDto);
     }
 
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping
-    public UserModel createUser(@RequestBody UserDto userDto) {
+    @Override
+    public UserModel createUser(UserDto userDto) {
         log.info("creating user{}", userDto);
         UserDto user =  userService.createUser(userDto);
         return userAssembler.toModel(user);
     }
 
-    @ResponseStatus(HttpStatus.OK)
-    @PutMapping(value = "/{emailUp}")
-    public UserModel updateUser(@PathVariable String emailUp, @RequestParam UserDto userDto) {
+    @Override
+    public UserModel updateUser(String emailUp, UserDto userDto) {
         log.info("Updating user with email{}", emailUp);
         UserDto user = userService.updateUser(emailUp, userDto);
         return userAssembler.toModel(user);
     }
 
-    @DeleteMapping(value = "/{emailDel}")
-    public ResponseEntity<Void> deleteUser(@PathVariable String emailDel) {
+    @Override
+    public ResponseEntity<Void> deleteUser(String emailDel) {
         log.info("Deleting user with email{}", emailDel);
         userService.deleteUser(emailDel);
         return ResponseEntity.noContent().build();
