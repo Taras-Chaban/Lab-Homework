94ef5708cd815928e8c8d6a3fb181e4481acc12b
Taras-Chaban <gold.bender2001@gmail.com>
2021-05-02 05:59:08 +0300

    modified InvoiceController

--- Homework5/src/main/java/com/lab/homework5/service/api/InvoiceApi.java
+++ Homework5/src/main/java/com/lab/homework5/service/api/InvoiceApi.java
@@ -1,5 +1,6 @@
 package com.lab.homework5.service.api;
 
+import com.lab.homework5.service.controller.model.InvoiceModel;
 import com.lab.homework5.service.dto.InvoiceDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,15 +9,15 @@ import org.springframework.web.bind.annotation.*;
 public interface InvoiceApi {
     @ResponseStatus(HttpStatus.OK)
     @GetMapping(value = "/{code}")
-    InvoiceDto getInvoice(@PathVariable Long code);
+    InvoiceModel getInvoice(@PathVariable Long code);
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    InvoiceDto createInvoice(@RequestBody InvoiceDto invoiceDto);
+    InvoiceModel createInvoice(@RequestBody InvoiceDto invoiceDto);
 
     @ResponseStatus(HttpStatus.OK)
     @PutMapping(value = "/{code}")
-    InvoiceDto updateInvoice(@PathVariable Long code, @RequestParam InvoiceDto invoiceDto);
+    InvoiceModel updateInvoice(@PathVariable Long code, @RequestParam InvoiceDto invoiceDto);
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping(value = "/{code}")
--- Homework5/src/main/java/com/lab/homework5/service/controller/InvoiceController.java
+++ Homework5/src/main/java/com/lab/homework5/service/controller/InvoiceController.java
@@ -1,11 +1,12 @@
 package com.lab.homework5.service.controller;
 
 import com.lab.homework5.service.api.InvoiceApi;
+import com.lab.homework5.service.controller.assembler.InvoiceAssembler;
+import com.lab.homework5.service.controller.model.InvoiceModel;
 import com.lab.homework5.service.dto.InvoiceDto;
 import com.lab.homework5.service.service.impl.InvoiceServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,23 +16,27 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class InvoiceController implements InvoiceApi {
     private final InvoiceServiceImpl invoiceService;
+    private final InvoiceAssembler invoiceAssembler;
 
     @Override
-    public InvoiceDto getInvoice(Long code) {
+    public InvoiceModel getInvoice(Long code) {
         log.info("Getting invoice with code {}", code);
-        return invoiceService.getInvoice(code);
+        InvoiceDto invoiceDto = invoiceService.getInvoice(code);
+        return invoiceAssembler.toModel(invoiceDto);
     }
 
     @Override
-    public InvoiceDto createInvoice(InvoiceDto invoiceDto) {
+    public InvoiceModel createInvoice(InvoiceDto invoiceDto) {
         log.info("Creating invoice {}", invoiceDto);
-        return invoiceService.createInvoice(invoiceDto);
+        InvoiceDto resultInvoiceDto = invoiceService.createInvoice(invoiceDto);
+        return invoiceAssembler.toModel(resultInvoiceDto);
     }
 
     @Override
-    public InvoiceDto updateInvoice(Long code, InvoiceDto invoiceDto) {
+    public InvoiceModel updateInvoice(Long code, InvoiceDto invoiceDto) {
         log.info("Updating invoice with code {}", code);
-        return invoiceService.updateInvoice(code, invoiceDto);
+        InvoiceDto resultInvoiceDto = invoiceService.updateInvoice(code, invoiceDto);
+        return invoiceAssembler.toModel(resultInvoiceDto);
     }
 
     @Override
