aa329522c5e40294803ac6e2279782ad71ae40db
Taras-Chaban <gold.bender2001@gmail.com>
2021-05-07 06:20:03 +0300

    add createUser (TestRestController)

--- Homework5/src/main/java/com/lab/homework5/service/controller/TestRestController.java
+++ Homework5/src/main/java/com/lab/homework5/service/controller/TestRestController.java
@@ -1,24 +1,31 @@
 package com.lab.homework5.service.controller;
 
 import com.lab.homework5.service.controller.model.UserModel;
+import com.lab.homework5.service.dto.UserDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/rest-template")
 public class TestRestController {
     private final RestTemplate restTemplate;
 
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping("/rest-template/{email}")
+    @GetMapping("/{email}")
     public UserModel getUser(@PathVariable String email) {
         String url = "http://localhost:8080/api/v1/users/" + email;
         return restTemplate.getForObject(url, UserModel.class);
     }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public UserModel createUser(@RequestBody UserDto userDto) {
+        String url = "http://localhost:8080/api/v1/users";
+        return restTemplate.postForObject(url, userDto, UserModel.class);
+    }
+
 }
