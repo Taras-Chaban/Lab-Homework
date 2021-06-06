e20823e61466702979e9a2c8620b46d7851fb1dc
Taras-Chaban <gold.bender2001@gmail.com>
2021-06-06 19:21:50 +0300

    modified

---
+++ CinemaMicroservice/src/main/java/com/cinemamicroservice/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.cinemamicroservice;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CinemaMicroserviceApplication.class);
+    }
+
+}
